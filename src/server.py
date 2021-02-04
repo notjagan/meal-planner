5649c0c6e8fc6168c7caec828b2361437f4ae098
Jagan Prem <notjagan@gmail.com>
2021-02-04 07:05:12 -0500

    Use SSL certification for server

--- src/server.py
+++ src/server.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 
+import ssl
+import pathlib
 import asyncio
+
 import websockets
 
 PORT = 0xf00d
@@ -13,7 +16,14 @@ async def handshake(websocket, path):
     print("Connection terminated.")
 
 
-start_server = websockets.serve(handshake, "0.0.0.0", PORT)
+ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLS_SERVER)
+certfile = pathlib.Path(__file__).with_name("cert.pem")
+keyfile = pathlib.Path(__file__).with_name("key.pem")
+ssl_context.load_cert_chain(certfile, keyfile)
+
+start_server = websockets.serve(
+    handshake, "0.0.0.0", PORT, ssl=ssl_context
+)
 
 asyncio.get_event_loop().run_until_complete(start_server)
 asyncio.get_event_loop().run_forever()
